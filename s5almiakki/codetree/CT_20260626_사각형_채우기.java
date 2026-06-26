8bfbc6d766d9ea5cbde0165fad9df0a7320013c8
5almiakki <jpui9635@gmail.com>
2026-06-26 14:00:05 +0900

    코드트리 사각형 채우기 DP로 풀이

---
+++ s5almiakki/codetree/CT_20260626_사각형_채우기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class CT_20260626_사각형_채우기 {
+
+    public class Main {
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            if (n == 1) {
+                System.out.print(1);
+                return;
+            }
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            }
+            System.out.print(dp[n]);
+        }
+
+    }
+
+}
